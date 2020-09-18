cc690a3999dfee8c804059fce9bed6c37e2e702b
Leonid K <l.klimparskiy@webprofy.ru>
2020-09-18 03:39:15 +0300

    add in t_module.php

--- t_module.php
+++ t_module.php
@@ -2,10 +2,10 @@
 # test module
 
 function testUI_NAME($name){
-    if(isset($name) && $name != '') return $name;
+    if(isset($name) && $name != '') return $name; else return false;
 }
 
 function testUI_EMAIL($mail){
-    if(isset($mail) && $mail != '') return $mail;
+    if(isset($mail) && $mail != '') return $mail; else return false;
 }
 ?>
